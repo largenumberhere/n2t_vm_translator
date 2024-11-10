016ad18a97ccbb4b3fdde8d0d5abf3b386d0024f
largenumberhere <largeNumbergoeshere@gmail.com>
2024-11-10 17:33:34 +1100

    Added required output path handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::path::Path;
+use rusty_parser::str;
+
 mod parser;
 mod writer;
 mod emit_asm;
@@ -10,9 +13,32 @@ use crate::writer::CodeWriter;
 fn main() {
     let mut args = std::env::args();
     let _ = args.next();
-    let file_in = std::fs::File::open(args.next().expect("Please specify an input file path")).expect("Failed to open input file");
-    let file_out = std::fs::File::create(args.next().expect("Please specify output file")).expect("Failed to open output file");
-    
+    let arg1 = args.next().unwrap();
+    // println!("{:?}", arg1);
+
+    let path = Path::new(&arg1);
+    let ext = path.extension().unwrap_or_default().to_str().unwrap();
+    let rest = path.to_str().unwrap();
+
+    let mut new_str = String::new();
+    for c in rest.chars() {
+        new_str.push(c);
+    }
+
+    // discard any file extension
+    if ext.len() > 0 {
+        for _ in 0.. ext.chars().count()+1 {
+            _ = new_str.pop()
+        }
+    }
+
+    // add .asm extenions
+    new_str.push_str(".asm");
+    let out_path = new_str;
+
+    let file_out = std::fs::File::create(&out_path).expect("Failed to open output file");
+    println!("{}", out_path);
+    let file_in = std::fs::File::open(&arg1).expect("Failed to open input file");
     let mut reader = parser::Parser::new(file_in);
     let mut writer: CodeWriter<File> = writer::CodeWriter::new(file_out);
 
